ab15f685d8170f4a1ad3b8fa8f9c89fb752dce0b
matteotagliatti <matteotagliatti@gmail.com>
2022-05-04 17:59:58 +0200

    add loader before spotify tracks are loaded

--- src/components/Spotify.vue
+++ src/components/Spotify.vue
@@ -2,12 +2,20 @@
   <div class="spotify">
     <div class="container">
       <h2>Music</h2>
-      <div class="grid">
+      <div class="grid" v-if="spotify">
         <SpotifyElement
           v-for="(song, index) in spotify"
           :song="song"
           :key="index"
         />
+      </div>
+      <div v-else class="loading">
+        <div class="lds-ellipsis">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
     </div>
   </div>
@@ -73,6 +81,67 @@ export default {
   .grid {
     @include grid(2);
     gap: 1rem;
+  }
+}
+
+/* Loading Spinner */
+.loading {
+  @include flex(row, center, center);
+}
+
+.lds-ellipsis {
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+}
+.lds-ellipsis div {
+  position: absolute;
+  top: 33px;
+  width: 13px;
+  height: 13px;
+  border-radius: 50%;
+  background: #d0d0d1;
+  animation-timing-function: cubic-bezier(0, 1, 1, 0);
+}
+.lds-ellipsis div:nth-child(1) {
+  left: 8px;
+  animation: lds-ellipsis1 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(2) {
+  left: 8px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(3) {
+  left: 32px;
+  animation: lds-ellipsis2 0.6s infinite;
+}
+.lds-ellipsis div:nth-child(4) {
+  left: 56px;
+  animation: lds-ellipsis3 0.6s infinite;
+}
+@keyframes lds-ellipsis1 {
+  0% {
+    transform: scale(0);
+  }
+  100% {
+    transform: scale(1);
+  }
+}
+@keyframes lds-ellipsis3 {
+  0% {
+    transform: scale(1);
+  }
+  100% {
+    transform: scale(0);
+  }
+}
+@keyframes lds-ellipsis2 {
+  0% {
+    transform: translate(0, 0);
+  }
+  100% {
+    transform: translate(24px, 0);
   }
 }
 </style>
